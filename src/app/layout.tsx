63e430156e8b451d2bce7bfcfa5a3eeb33ce068d
kks940104 <kks940104@gmail.com>
2025-02-06 18:19:35 +0900

    초기 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import StyledComponentsRegistry from './registry'
+import Header from './global/ui/outlines/Header'
+import Footer from './global/ui/outlines/Footer'
+import { CommonProvider } from './global/contexts/CommonContext'
+import { Metadata } from 'next'
+import './globals.css'
+import 'react-datepicker/dist/react-datepicker.css'
+
+export const metadata: Metadata = {
+  title: '핀테크 과정 포트폴리오',
+  description: '설명..',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ko">
+      <body>
+        <StyledComponentsRegistry>
+          <CommonProvider>
+            <Header />
+            <main className="main-centent">{children}</main>
+            <Footer />
+          </CommonProvider>
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  )
+}
